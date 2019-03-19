963d57b15ade793295368c364b88c933b5a2e1c7
Zhuanglijuan <1330129868@qq.com>
2019-03-19 14:24:08 +0800

    初始化

---
+++ src/app.jsx
@@ -0,0 +1,18 @@
+/*
+ * @Author: samantha 
+ * @Date: 2019-03-19 11:08:05 
+ * @Last Modified by: samantha
+ * @Last Modified time: 2019-03-19 14:09:37
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'font-awesome/css/font-awesome.min.css'
+import './index.css';
+import './index.scss';
+
+ReactDOM.render(
+  <div>
+    <h1>Hello, world!</h1>
+  </div>,
+  document.getElementById('app')
+);
