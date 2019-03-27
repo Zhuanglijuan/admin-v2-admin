3214ddaa97442828f0a7ffdafc993116023ef68c
Zhuanglijuan <1330129868@qq.com>
2019-03-27 15:21:45 +0800

    order

--- src/app.jsx
+++ src/app.jsx
@@ -10,11 +10,13 @@ import {BrowserRouter as Router, Switch, Redirect, Route, Link} from 'react-rout
 
 import Layout from 'component/layout/index.jsx';
 // 页面
-import Home from 'page/home/index.jsx';
-import ProductRouter from 'page/product/router.jsx';
-import Login from 'page/login/index.jsx';
-import UserList from 'page/user/index.jsx';
-import ErrorPage from 'page/error/index.jsx';
+import Home             from 'page/home/index.jsx';
+import ProductRouter    from 'page/product/router.jsx';
+import Login            from 'page/login/index.jsx';
+import OrderList        from 'page/order/index.jsx';
+import OrderDetail      from 'page/order/detail.jsx';
+import UserList         from 'page/user/index.jsx';
+import ErrorPage        from 'page/error/index.jsx';
 
 class App extends React.Component {
     render() {
@@ -24,7 +26,10 @@ class App extends React.Component {
                     <Route exact path="/" component={Home}/>
                     <Route path="/product" component={ProductRouter}/>
                     <Route path="/product-category" component={ProductRouter}/>
+                    <Route path="/order/index" component={OrderList}/>
+                    <Route path="/order/detail/:orderNumber" component={OrderDetail}/>
                     <Route path="/user/index" component={UserList}/>
+                    <Redirect exact from="/order" to="/order/index"/>
                     <Redirect exact from="/user" to="/user/index"/>
                     <Route component={ErrorPage}/>
                 </Switch>
