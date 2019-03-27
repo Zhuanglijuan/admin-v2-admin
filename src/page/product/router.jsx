c02bfde53b34a3848df26cc192681db820cc19e5
Zhuanglijuan <1330129868@qq.com>
2019-03-27 14:39:24 +0800

    category

--- src/page/product/router.jsx
+++ src/page/product/router.jsx
@@ -5,9 +5,11 @@ import React from 'react';
 import {BrowserRouter as Router, Switch, Redirect, Route, Link} from 'react-router-dom';
 
 // 页面
-import ProductList from 'page/product/index/index.jsx';
-import ProductSave from 'page/product/index/save.jsx';
+import ProductList      from 'page/product/index/index.jsx';
+import ProductSave      from 'page/product/index/save.jsx';
 import ProductDetail    from 'page/product/index/detail.jsx';
+import CategoryList     from 'page/product/category/index.jsx';
+import CategoryAdd      from 'page/product/category/add.jsx';
 
 class ProductRouter extends React.Component {
     render() {
@@ -16,7 +18,10 @@ class ProductRouter extends React.Component {
                 <Route path="/product/index" component={ProductList}/>
                 <Route path="/product/save/:pid?" component={ProductSave}/>
                 <Route path="/product/detail/:pid" component={ProductDetail}/>
-                <Redirect redirect exact from="/product" to="/product/index"/>
+                <Route path="/product-category/index/:categoryId?" component={CategoryList}/>
+                <Route path="/product-category/add" component={CategoryAdd}/>
+                <Redirect exact from="/product" to="/product/index"/>
+                <Redirect exact from="/product-category" to="/product-category/index"/>
             </Switch>
         );
     }
